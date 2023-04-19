6f000860f0e34e83e458e15f63c65408f015b849
CodeCharlieAdele <charlotteadeletn@gmail.com>
2023-04-19 15:23:13 -0400

    added bond activity

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("Please enter your first name: ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("Thank you, now please enter your last name: ");
+        string lastName = Console.ReadLine();
+        
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+
     }
 }
