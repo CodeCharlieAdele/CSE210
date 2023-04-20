18fb62eb4e21497b0966ad00633bc7c2a2bc1134
CodeCharlieAdele <charlotteadeletn@gmail.com>
2023-04-20 11:37:09 -0400

    fixed where everything is

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,51 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        //Console.Write("Please enter your first name: ");
-        //string firstName = Console.ReadLine();
+        Console.Write("Please enter your first name: ");
+        string firstName = Console.ReadLine();
 
-        //Console.Write("Thank you, now please enter your last name: ");
-        //string lastName = Console.ReadLine();
+        Console.Write("Thank you, now please enter your last name: ");
+        string lastName = Console.ReadLine();
 
-        //Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
 
-
-        Console.Write("Please enter your GPA: ");
-        string userGpa = Console.ReadLine();
-        int gpa = int.Parse(userGpa);
-
-        string letterGrade = ("");
-
-        if (gpa >= 90)
-        {
-            letterGrade = "A";
-        }
-        else if (gpa >= 80)
-        {
-            letterGrade = "B";
-        }
-        else if (gpa >= 70)
-        {
-            letterGrade = "C";
-        }
-        else if (gpa >= 60)
-        {
-            letterGrade = "D";
-        }
-        else
-        {
-            letterGrade = "F";
-        }
-        if (gpa >= 70)
-        {
-            Console.WriteLine("You passed!");
-        }
-        else
-        {
-            Console.WriteLine("You failed :( ");
-        }
-
-        Console.WriteLine($"Your grade is {letterGrade}");
 
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your GPA: ");
+        string userGpa = Console.ReadLine();
+        int gpa = int.Parse(userGpa);
+
+        string letterGrade = ("");
+
+        if (gpa >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (gpa >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (gpa >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (gpa >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+        if (gpa >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You failed :( ");
+        }
+
+        Console.WriteLine($"Your grade is {letterGrade}");
     }
 }
