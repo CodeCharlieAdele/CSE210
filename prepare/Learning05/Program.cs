3f17d0d96f101c4169fc6f0866a56fe0e8d70136
CodeCharlieAdele <charlotteadeletn@gmail.com>
2023-05-12 13:20:05 -0400

    Shape activity Completed

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Pink", 3);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Purple", 4, 5);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Blue", 6);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            // Notice that all shapes have a GetColor method from the base class
+            string color = s.GetColor();
+
+            // Notice that all shapes have a GetArea method, but the behavior is
+            // different for each type of shape
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
